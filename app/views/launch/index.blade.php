7e984e9be898d473e504287a4170d72d8341eda0
murum <cr222cs@student.lnu.se>
2014-10-01 19:40:53 +0200

    update spelling of CS:GO

--- app/views/launch/index.blade.php
+++ app/views/launch/index.blade.php
@@ -8,7 +8,7 @@
     <div class="col-xs-12 col-sm-8 content">
       <h1>It's time to take gaming seriously</h1>
       <p>
-        We are currently building the easiest solution to search for other players and teams in any game. Counter Strike Global Offensive and Dota 2 will be the first two games that we will focus on.
+        We are currently building the easiest solution to search for other players and teams in any game. Counter Strike: Global Offensive and Dota 2 will be the first two games that we will focus on.
       </p>
       <p>
         We want to help you find the right people for your team and match you with other players in your region and skill.
