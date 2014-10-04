e911a3fb7156e2901504668dd384c52c98f4e49d
murum <cr222cs@student.lnu.se>
2014-10-04 04:12:40 +0200

    update LAUNCH environment variable in routes

--- app/routes.php
+++ app/routes.php
@@ -10,7 +10,7 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-if(getenv('LAUNCH')) {
+if(getenv('LAUNCH') && getenv('LAUNCH' !== 'false')) {
 	Route::get('/', array('as' => 'launch', 'uses' => 'LaunchController@index'));
 	Route::post('/', array('as' => 'launch', 'uses' => 'LaunchController@store'));
 } else {
